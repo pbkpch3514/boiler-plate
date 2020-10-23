a94a47d50f61384f2c6ce552af4e28699866ccc8
pbkpch <pbkpch@kookmin.ac.kr>
2020-10-23 18:33:14 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
 
+const config = require('./config/key')
+
+const {User} = require("./models/User")
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://pbkpch:pbk3514421@boiler-plate.acfva.mongodb.net/boiler-plate?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
 
-app.get('/', (req, res) => res.send('Hello World! 안녕하세요~~'))
+app.get('/', (req, res) => res.send('정글차이, 정글엔 개가 살고있다, 모두 행복하세요. 이 맞짱깔새끼야'))
+
+app.post('/register', (req, res) => {
+    // 회원 가입 시 필요 정보
+    // 그것들을 데이터 베이스에 넣어준다.
+    
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
